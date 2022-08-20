584bbc655ae3a67189399397f0ad10aefd460577
Diana Gabriela Miños <65367714+dgminos@users.noreply.github.com>
2022-08-20 01:05:17 -0300

    Update database.js
    Change port  variable name

--- src/db/database.js
+++ src/db/database.js
@@ -5,7 +5,7 @@ export const db = mysql2.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    port: process.env.PORT,
+    port: process.env.DBPORT,
     database: process.env.DATABASE,
 })
 db.connect((err) =>{
